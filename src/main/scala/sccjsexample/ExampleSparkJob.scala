9247ade64d78257afbe102d44473cdc902aced2d
Xian Teng <xian.teng@datastax.com>
2015-07-15 12:39:55 -0500

    added more complex CFS join example

--- src/main/scala/sccjsexample/ExampleSparkJob.scala
+++ src/main/scala/sccjsexample/ExampleSparkJob.scala
@@ -3,14 +3,22 @@ package sccjsexample
 import com.datastax.spark.connector._
 import com.datastax.spark.connector.cql.CassandraConnector
 import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
 import spark.jobserver.{SparkJob, SparkJobValid, _}
 import com.typesafe.config.Config
 
 
 object ExampleSparkJob extends SparkJob {
+
+  case class KV(k: Int, v: Int)
+
   override def runJob(sc: SparkContext, jobConfig: Config): Any = {
     val con = CassandraConnector(sc.getConf)
     con.withSessionDo({ session =>
+      session.execute(
+        """DROP KEYSPACE IF EXISTS test"""
+      )
+
       session.execute(
         """CREATE KEYSPACE IF NOT EXISTS test
           |WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1 }""".stripMargin)
@@ -27,19 +35,14 @@ object ExampleSparkJob extends SparkJob {
     })
 
     sc.cassandraTable("test", "kv").saveToCassandra("test", "copykv")
-    sc.cassandraTable("test", "copykv").take(10)
 
     //dse hadoop fs -mkdir /test_spark_sql_cfs
-    //dse hadoop fs -copyFromLocal people.json /test_spark_sql_cfs/
-    val sqlc = new org.apache.spark.sql.SQLContext(sc)
-    import sqlc._
-    import sqlc.implicits._
-    val inpath = "cfs:///test_spark_sql_cfs/people.json"
-    val cfs_people = sqlc.jsonFile(inpath)
-    cfs_people.printSchema()
-    cfs_people.registerTempTable("cfs_people")
-    val cfs_teenagers = sqlc.sql("SELECT name FROM cfs_people WHERE age >= 13 AND age <= 19")
-    cfs_teenagers.collect().foreach(println)
+    //dse hadoop fs -copyFromLocal example.txt /test_spark_sql_cfs/
+    val textFile = sc.textFile("/test_spark_sql_cfs/example.txt")
+    val data = textFile.map(line => line.split(" ").map(elem => elem.trim).map(elem => elem.toInt))
+    val dataBy1 = data.map(row => (row(0), row(1))).keyBy(row => row._1)
+    val kvByk = sc.cassandraTable[KV]("test", "kv").keyBy(kv => kv.k)
+    dataBy1.join(kvByk).collect()
   }
 
   override def validate(sc: SparkContext, config: Config): SparkJobValidation = {
